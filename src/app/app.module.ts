6c9969959101a732e4a437969bf79fc22d403fc5
SumitK27 <sumitkajbaje@gmail.com>
2023-03-26 19:10:47 +0530

    Contact Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactComponent } from './contact/contact.component';
 import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    ContactComponent,
     HeroComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
